bfd877d94d0f5b3f346cd8e7c74c8c61998e9438
Yoann FRANCOIS <yoann.francois@hotmail.fr>
2023-08-31 14:39:30 +0100

    Premier commit

---
+++ ProjetAnnuaire/src/main/java/CDA26Grp4/ProjetAnnuaire/SystemInfo.java
@@ -0,0 +1,13 @@
+package CDA26Grp4.ProjetAnnuaire;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ ProjetAnnuaire/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module CDA26Grp4.ProjetAnnuaire {
+    requires javafx.controls;
+    exports CDA26Grp4.ProjetAnnuaire;
+}
